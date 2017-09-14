5c0eb29acc5e15be61d111c7b862f9d52ebb65dd
Chris Nunez <cenunez47@gmail.com>
2017-09-14 23:18:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "My name is Chris"
+  end
+
+  get '/' do
+    "My hometown is Novato"
+  end
+
+  get '/' do
+    "My favorite song is Everlong."
+  end
+  
 end
